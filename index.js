27bcd1e1194d1d543b5c263eea1db41ed399fb69
Poorvi <pjnayak2003@gmail.com>
2023-09-05 19:06:10 +0530

    Finished syncing blocks

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const express = require("express");
+const request = require("request");
 const Blockchain = require("./blockchain");
 const PubSub = require("./publishsubscribe");
 
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({blockchain});
+
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => pubsub.broadcastChain(), 500);
 
@@ -21,14 +25,23 @@ app.post("/api/mine", (req,res)=>{
     res.redirect('/api/blocks')
 });
 
-const DEFAULT_PORT = 3000;
 let PEER_PORT;
 
 if(process.env.GENERATE_PEER_PORT === 'true'){
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random()*1000);
 }
 
+const syncChains = ()=>{
+    request({url:`${ROOT_NODE_ADDRESS}/api/blocks`}, (error,response,body)=>{
+        if(!error && response.statusCode===200){
+            const rootChain = JSON.parse(body);
+            blockchain.replaceChain(rootChain);
+        }
+    })
+
+}
 const PORT = PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, ()=>{
     console.log(`listening to PORT ${PORT}`);
+    syncChains();
 })
